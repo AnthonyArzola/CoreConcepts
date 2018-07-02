7ff28cb9526b150b55da3fc9f12caa41d11854e8
Anthony Arzola <AnthonyArzola@gmail.com>
2018-07-01 21:26:20 -0700

    Implement Quick Sort.

---
+++ CoreConcepts.Tests/Algorithms/Sorting/QuickSortTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+using CoreConcepts.Algorithms.Sorting;
+
+using Xunit;
+
+namespace CoreConcepts.Tests.Algorithms.Sorting
+{
+    public class QuickSortTests
+    {
+        [Fact]
+        public void Test_Sorting_Empty_Array()
+        {
+            int[] array = new int[] { };
+
+            try
+            {
+                QuickSort.Sort(array);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Assert.True(false, $"Unable to perform sort on empty array. Exception is: {ex.Message}");
+            }
+
+            Assert.Empty(array);
+        }
+
+        [Fact]
+        public void Test_Sorting()
+        {
+            // Sort using CoreConcepts implementation
+            int[] unsortedArray = new int[] { 300, 5, 1, 8, 100, 2, 10 };
+            QuickSort.Sort(unsortedArray);
+
+            // Sort using framework implementation
+            int[] unsortedArray2 = new int[] { 300, 5, 1, 8, 100, 2, 10 };
+            Array.Sort(unsortedArray2);
+
+            // Compare results
+            Assert.True(unsortedArray.SequenceEqual(unsortedArray2), "Sorted arrays do not match.");
+        }
+
+        [Fact]
+        public void Test_Sorting2()
+        {
+            // Sort using CoreConcepts implementation
+            int[] unsortedArray = new int[] { 38, 27, 43, 3, 9, 82, 10 };
+            QuickSort.Sort(unsortedArray);
+
+            // Sort using framework implementation
+            int[] unsortedArray2 = new int[] { 38, 27, 43, 3, 9, 82, 10 };
+            Array.Sort(unsortedArray2);
+
+            // Compare results
+            Assert.True(unsortedArray.SequenceEqual(unsortedArray2), "Sorted arrays do not match.");
+        }
+    }
+}
---
+++ CoreConcepts/Algorithms/Sorting/QuickSort.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace CoreConcepts.Algorithms.Sorting
+{
+    public class QuickSort
+    {
+        public static void Sort(int[] array)
+        {
+            if (array.Length == 0)
+                return;
+
+            Sort(array, 0, array.Length - 1);
+        }
+
+        private static void Sort(int[] array, int lowIndex, int highIndex)
+        {
+            if (lowIndex < highIndex)
+            {
+                int partitionIndex = Partition(array, lowIndex, highIndex);
+                Sort(array, lowIndex, partitionIndex - 1); // Before partition index
+                Sort(array, partitionIndex + 1, highIndex); // After partition index
+            }
+        }
+
+        private static int Partition(int[] array, int lowIndex, int highIndex)
+        {
+            // Select last element as pivot value
+            int pivotValue = array[highIndex];
+            
+            // Lowest element index
+            int lowestIndex = lowIndex;
+            int temp;
+
+            // Iterate from low to high, but exclude pivot by subtracting 1.
+            for (int index = lowIndex; index <= highIndex - 1; index++)
+            {
+                // If element is less than pivot, move element to the left
+                if (array[index] <= pivotValue)
+                {
+                    // Swap current element with lowest element
+                    temp = array[lowestIndex];
+                    array[lowestIndex] = array[index];
+                    array[index] = temp;
+
+                    // Increment lowestIndex
+                    lowestIndex++;
+                }
+            }
+
+            // Move pivot into new position (where lowest index left off)
+            temp = array[lowestIndex];
+            array[lowestIndex] = array[highIndex];
+            array[highIndex] = temp;
+
+            // Return calculated partition (pivot) index
+            return lowestIndex;
+        }
+
+    }
+}
