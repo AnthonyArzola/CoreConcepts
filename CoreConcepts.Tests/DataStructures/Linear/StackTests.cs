69410360df0360b0d5bb77bf5c6855433aded5fe
Anthony Arzola <AnthonyArzola@gmail.com>
2020-04-21 01:09:39 -0700

    Implement Stack tests.

---
+++ CoreConcepts.Tests/DataStructures/Linear/StackTests.cs
@@ -0,0 +1,63 @@
+﻿using Core = CoreConcepts.DataStructures.Linear;
+
+using Xunit;
+
+namespace CoreConcepts.Tests.DataStructures.Linear
+{
+    public class StackTests
+    {
+        [Fact]
+        public void Test_Push()
+        {
+            Core.Stack<int> stack = new Core.Stack<int>();
+            int itemsToPush = 5;
+            for (int i = 0; i < itemsToPush; i++)
+            {
+                stack.Push(i);
+            }
+            Assert.Equal(itemsToPush, stack.Count());
+        }
+
+        [Fact]
+        public void Test_Peek()
+        {
+            Core.Stack<int> stack = new Core.Stack<int>();
+            int pushedValue = 5;
+            stack.Push(pushedValue);
+            Assert.Equal(pushedValue, stack.Peek());
+        }
+
+        [Fact]
+        public void Test_Pop()
+        {
+            Core.Stack<int> stack = new Core.Stack<int>();
+            int itemsToPush = 5;
+            for (int i = 1; i <= itemsToPush; i++)
+            {
+                stack.Push(i);
+            }
+            for (int i = itemsToPush; i > 0; i--)
+            {
+                int value = stack.Pop();
+                Assert.Equal(i, value);
+            }
+
+            Assert.Equal(0, stack.Count());
+        }
+
+        [Fact]
+        public void Test_Clear()
+        {
+            Core.Stack<int> stack = new Core.Stack<int>();
+            int itemsToPush = 5;
+            for (int i = 0; i < itemsToPush; i++)
+            {
+                stack.Push(i);
+            }
+            stack.Clear();
+
+            Assert.Equal(0, stack.Count());
+        }
+
+    }
+}
