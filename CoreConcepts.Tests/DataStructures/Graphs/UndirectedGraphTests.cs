60230470da56a30e43d3942f87952ffb05866cb4
Anthony Arzola <AnthonyArzola@gmail.com>
2020-05-03 22:32:56 -0700

    Add DFS test

--- CoreConcepts.Tests/DataStructures/Graphs/UndirectedGraphTests.cs
+++ CoreConcepts.Tests/DataStructures/Graphs/UndirectedGraphTests.cs
@@ -27,15 +27,32 @@ namespace CoreConcepts.Tests.DataStructures.Graphs
 
             UndirectedGraph graph = new UndirectedGraph(vertices, edges);
 
-            var dfsResult = graph.DepthFirstSearch(1, 6);
-            Assert.True(dfsResult.vertexFound);
-            Console.Write("DFS:");
-            Console.WriteLine(ListToString(dfsResult.visitedVertices));
-
-            var bfsResult = graph.BreadthFirstSearch(1, 6);
-            Assert.True(bfsResult.vertexFound);
+            var (vertexFound, visitedVertices) = graph.BreadthFirstSearch(1, 6);
+            Assert.True(vertexFound);
+            Assert.NotEmpty(visitedVertices);
             Console.WriteLine("BFT:");
-            Console.WriteLine(ListToString(bfsResult.visitedVertices));
+            Console.WriteLine(ListToString(visitedVertices));
+        }
+
+        [Fact]
+        public void Test_Undirected_Graph_DFS()
+        {
+            // Define vertices
+            int[] vertices = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            // Define edges
+            (int leftVertex, int rightVertex)[] edges = new[] {
+                (1,2), (1,3),
+                (2,4), (3,5),
+                (3,6), (4,7),
+                (5,7), (5,8),
+                (5,6), (8,9),
+                (9,10)};
+
+            UndirectedGraph graph = new UndirectedGraph(vertices, edges);
+            var (vertexFound, visitedVertices) = graph.DepthFirstSearch(1, 10);
+            Assert.True(vertexFound);
+            Assert.NotEmpty(visitedVertices);
         }
 
         public static string ListToString(List<int> list)
